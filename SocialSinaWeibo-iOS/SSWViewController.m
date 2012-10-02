3f55604909c947fabc494e0a6a4d1495996aa8a1
James Chen <ashchan@gmail.com>
2012-10-02 15:58:05 +0900

    send a status with SLComposeViewController

--- SocialSinaWeibo-iOS/SSWViewController.m
+++ SocialSinaWeibo-iOS/SSWViewController.m
@@ -6,6 +6,8 @@
 //  Copyright (c) 2012 ashchan.com. All rights reserved.
 //
 
+#import <Accounts/Accounts.h>
+#import <Social/Social.h>
 #import "SSWViewController.h"
 
 @interface SSWViewController ()
@@ -25,5 +27,20 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)saySomething:(id)sender {
+    if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeSinaWeibo]) {
+        SLComposeViewController *composeViewController = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeSinaWeibo];
+        [composeViewController setInitialText:@"无痛呻吟几下吧。iOS 6 微博集成示例。"];
+        [composeViewController addURL:[NSURL URLWithString:@"https://github.com/ashchan/SocialSinaWeibo"]];
+        [composeViewController setCompletionHandler:^(SLComposeViewControllerResult result) {
+            // check result
+        }];
+
+        [self presentViewController:composeViewController animated:YES completion:nil];
+    }
+}
+
+//3496696657222748
 
 @end
